3c2bdc993d522ac2b67a2369b923b874687be601
amal <amalbouabdallahh1@gmail.com>
2020-11-01 18:54:39 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+// import dependencies
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import logger from "morgan";
+// set up dependencies
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(logger("dev"));
+// set up mongoose
+mongoose
+  .connect("mongodb://localhost/NodeProject")
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((error) => {
+    console.log("Error connecting to database");
+  });
+// set up port
+const port = 5035;
+// set up route
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Welcome!",
+  });
+});
+app.listen(port, () => {
+  console.log(`Our server is running on port ${port}`);
+});
